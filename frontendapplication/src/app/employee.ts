ae008abf94469b3d65df46025a8ac4b1b1d03f24
pavan147 <pavanmgawande@gmail.com>
2025-01-11 22:20:33 +0530

    Create employee added

--- frontendapplication/src/app/employee.ts
+++ frontendapplication/src/app/employee.ts
@@ -1,13 +1,13 @@
 export class Employee {
-    id: number;
-    firstName: string;
-    lastName: string;
-    emailId: string;
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    emailId?: string;
   
-    constructor(id: number, firstName: string, lastName: string, emailId: string) {
-      this.id = id;
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.emailId = emailId;
-    }
+//     constructor(id: number, firstName: string, lastName: string, emailId: string) {
+//       this.id = id;
+//       this.firstName = firstName;
+//       this.lastName = lastName;
+//       this.emailId = emailId;
+//     }
 }
