40df19fde71af227a7fd11d38577e0a52fc2bbf3
pavan147 <pavanmgawande@gmail.com>
2025-01-11 22:20:33 +0530

    employeeList coponent

--- frontendapplication/src/app/employee-list/employee-list.component.ts
+++ frontendapplication/src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,9 +11,16 @@ export class EmployeeListComponent implements OnInit {
 
   employees: Employee[]= [];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
   }
 
 }
---
+++ frontendapplication/src/app/employee.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Employee } from './employee';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+   private baseUrl = "http://localhost:8080/api/v1/employees";
+  constructor(private httpClient: HttpClient) { }
+
+  getEmployeesList(): Observable<Employee[]>{
+    return this.httpClient.get<Employee[]>(`${this.baseUrl}`);
+  }
+
+  createEmployee(employee: Employee): Observable<Object>{
+    return this.httpClient.post(`${this.baseUrl}`, employee);
+  }
+}
